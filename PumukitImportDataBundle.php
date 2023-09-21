3f93abc3d20b45d3e677d6a7b45257c83f2420b9
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2023-09-21 09:45:17 +0200

    PHPCsFixer

--- PumukitImportDataBundle.php
+++ PumukitImportDataBundle.php
@@ -6,6 +6,4 @@ namespace Pumukit\ImportDataBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class PumukitImportDataBundle extends Bundle
-{
-}
+class PumukitImportDataBundle extends Bundle {}
